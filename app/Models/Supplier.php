9188f62fe57efab71530b37f7190d248bf3dbff8
Dung Nguyen Van <nobinguyen94@gmail.com>
2016-07-04 13:42:23 +0700

    create model user and supplier

---
+++ app/Models/Supplier.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+	protected $table = 'suppliers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    
+    protected $fillable = [
+        'name', 'description',
+    ];
+    
+    public $timestamps = false;
+}
