4227b772778148c52d6d430ca64d363cdc9e6db8
Son Tran <sontd.it@gmail.com>
2016-07-14 17:03:55 +0700

    Update Roles Table Seeder

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
   <div class="menu_section menu_fixed">
-    <h3>MANAGEMENT</h3>
+    <h3>{{ strtoupper(Auth::user()->role->name) }}</h3>
     <ul class="nav side-menu">
       <li><a><i class="fa fa-buysellads"></i> @lang('common.menu_sales') <span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
