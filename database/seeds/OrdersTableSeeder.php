184e3ddc64eeb66d7de1b6c1ca6200869e4f857b
Son Tran <sontd.it@gmail.com>
2016-07-31 23:04:11 +0700

    Clean unuse file

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -16,26 +16,26 @@ class OrdersTableSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        // Orders in the past
-        for ($i = 0; $i < 100; $i++) {
+        // 150 Orders in the past
+        for ($i = 0; $i < 150; $i++) {
             Order::create([
                 'user_id' => rand(1, 10),
                 'description' => $faker->realText($maxNbChars = 255, $indexSize = 2),
                 'supplier_id' => rand(1, 5),
-                'total_cost' => rand(1, 990) * 100,
+                'total_cost' => rand(1, 99) * 100,
                 'created_at' => $faker->dateTimeBetween($startDate = '-3 years', $endDate = 'now')
             ]);
         }
 
         // Orders today
         foreach (User::get() as $user) {
-            $amount = rand(0, 10);
+            $amount = rand(0, 3);
             for ($i = 0; $i < $amount; $i++) {
                 Order::create([
                     'user_id' => $user->id,
                     'description' => $faker->text($maxNbChars = 255),
                     'supplier_id' => rand(1, 5),
-                    'total_cost' => rand(1, 990) * 10,
+                    'total_cost' => rand(1, 99) * 100,
                 ]);
             }
         }
